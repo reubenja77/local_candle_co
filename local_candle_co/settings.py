fa80b7fb03b4ed7c9297a9463853d7b4323b80a1
Reuben <reubenabrahams77@gmail.com>
2025-11-25 00:15:27 +0200

    Fix: Remove db.sqlite3 before Heroku deployment

--- local_candle_co/settings.py
+++ local_candle_co/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
