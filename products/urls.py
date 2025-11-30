49f3437dfab4849ca8e30bc505206a8476e8d463
Reuben <reubenabrahams77@gmail.com>
2025-11-30 17:57:04 +0200

    Remove temporary 500 test route

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,4 @@ app_name = 'products'
 urlpatterns = [
     path('', views.product_list, name='list'),
     path('p/<slug:slug>/', views.product_detail, name='detail'),
-    path('error-test/', views.test_error, name='error_test'),
 ]
