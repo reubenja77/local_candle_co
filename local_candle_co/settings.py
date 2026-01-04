c1c820018abe2501485a5d4a97bba0b426055d28
Reuben <reubenabrahams77@gmail.com>
2026-01-04 22:34:27 +0200

    Switch production email to SMTP SSL

--- local_candle_co/settings.py
+++ local_candle_co/settings.py
@@ -199,10 +199,13 @@ if DEBUG:
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = os.environ.get("EMAIL_HOST", "")
-    EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
+    EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "465"))
     EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
     EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
-    EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
+
+    EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "False") == "True"
+    EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", "True") == "True"
+
     DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 
 
